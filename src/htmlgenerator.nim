5cb5cd757b5bc4e2c61479660ae791f6c1be76a3
z-kk <z-kk@gmail.com>
2022-03-15 21:26:12 +0900

    make select objects

--- src/htmlgenerator.nim
+++ src/htmlgenerator.nim
@@ -1,8 +1,8 @@
 import
-  htmlgenerator / [head, input, table, others, base]
+  htmlgenerator / [head, input, select, table, others, base]
 
 export
-  head, input, table, others
+  head, input, select, table, others
 
 export
   base.add
